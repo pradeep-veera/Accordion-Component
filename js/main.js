0dcba7d9c586dae331c66ba62a40d951ba3f0bb0
pradeep-veera <balaji.veera@st.ovgu.de>
2017-09-11 08:39:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 // JavaScript Document
-
+/*
+function load_file(file, element){
+    console.log(this.title);    
+	$(element).load(file);
+}
+*/
 $(document).ready(function() {
-    $('.button').click(function() {
-        $(this).find(".text").load("buffer.txt");
-        //$('.text').load("buffer.txt");
-        console.log($(this).find(".text").load("buffer.txt"));
+    $('.button').click(function(event) {
+        var button = $(this);
+        var status = $(this).attr('id');
+        var text = button.closest('div').attr('text');
     });
 });
+/*
+$(document).ready(function() {
+    $(".button").click(function() {
+        $.ajax({
+            url : "buffer.txt",
+            dataType: "text",
+            success : function (data) {
+                $("#text").html(data);
+            }
+        });
+    });
+}); */
