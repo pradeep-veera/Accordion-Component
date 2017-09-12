2a7ef9c41562d0b3e7f77351848f5da870b63c5c
Balaji Satya Pradeep Veera <balaji.veera@st.ovgu.de>
2017-09-12 15:26:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,49 +1,62 @@
-// JavaScript Document
 /*
-function load_file(file, element){
-    console.log(this);    
-	$(element).load(file);
-}
+Javascript file.
+
+Purpose: To load the content of the static file on activating the tabs.
 */
+
 $(document).ready(function() {
-    if (window.innerWidth <= 768) {
-        $('.button').click(function(event) {
+    // Check fot the width of the device
+    // Mobile device width (<= 768px)
+    $('.button').click(function() {   
+        var button = $(this);
+        var status = $(this).attr('id');
+
+        if($('.button').hasClass('active')){
+            $('.button').removeClass('active'); 
+            $(this).addClass('active');
+        }else {
+            $(this).addClass('active');
+        }        
+
+        if (window.innerWidth <= 768) {
             console.log("mobile");
             var button = $(this);
             var status = $(this).attr('id');
-            var text = $(this).next();
+            // Check the button id to load the content accordingly
             if(status == "button_1"){
+                // Load the static content for 1st tab
                 $('#mobile_content_1 div.text').load('buffer_1.html');
+                // Display only the div of 1st tab and hide div's of other tab's
+                $('#mobile_content_1').css('display','block');
+                $('#mobile_content_2').css('display','none');
+                $('#mobile_content_3').css('display','none');
             }else if(status == "button_2"){
+                 // Load the static content for 2nd tab
                 $('#mobile_content_2 div.text').load('buffer_2.html');
+                 // Display only the div of 2nd tab and hide div's of other tab's
+                $('#mobile_content_1').css('display','none');
+                $('#mobile_content_2').css('display','block');
+                $('#mobile_content_3').css('display','none');                
             }else if(status == "button_3"){
+                 // Load the static content for 3rd tab
                 $('#mobile_content_3 div.text').load('buffer_3.html');
+                 // Display only the div of 3rd tab and hide div's of other tab's
+                $('#mobile_content_1').css('display','none');
+                $('#mobile_content_2').css('display','none');
+                $('#mobile_content_3').css('display','block');                
             }
-        });
-    }else{
-        $('.button').click(function(event) {
-            console.log("desktop");
-            var button = $(this);
-            var status = $(this).attr('id');
+        }else {
+                // Check the id of the tab clicked.
             if(status == "button_1"){
+              // Load the content of the 1st tab
                 $('.text_container div.text').load('buffer_1.html');
             }else if(status == "button_2"){
+              // Load the content of the 2nd tab
                 $('.text_container div.text').load('buffer_2.html');
             }else if(status == "button_3"){
+                // Load the content of the 3rd tab
                 $('.text_container div.text').load('buffer_3.html');
-            }
-        }); 
-    }    
-});
-/*
-$(document).ready(function() {
-    $(".button").click(function() {
-        $.ajax({
-            url : "buffer.txt",
-            dataType: "text",
-            success : function (data) {
-                $("#text").html(data);
-            }
-        });
+            }                
+        }
     });
-}); */
+});
